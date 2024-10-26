fed1d11f793dcff7675875c5322e3de45bcb2e33
BoiGonMon <owencadewells@gmail.com>
2024-10-26 18:28:22 -0500

    improved movement and collision

--- ai-generated-game-title/Scenes/CharacterBody2d.cs
+++ ai-generated-game-title/Scenes/CharacterBody2d.cs
@@ -3,21 +3,23 @@ using System;
 
 public partial class CharacterBody2d : CharacterBody2D
 {
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-    }
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+	
+	[Export]
+	private int speed = 300;
 
-    [Export]
-    public int speed = 5;
+	public void GetInput()
+	{
+		Vector2 inputDir = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+		Velocity = inputDir * speed;
+	}
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta)
-    {
-        float tempSpeed = speed;
-        Vector2 velocity = new Vector2();
-        velocity = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down") * speed;
-
-        MoveAndCollide(velocity);
-    }
+	public override void _PhysicsProcess(double delta)
+	{
+		GetInput();
+		MoveAndSlide();
+	}
 }
