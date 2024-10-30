0e5a4f6e662084ce2ddfb6ac063c008e6951ee7d
BoiGonMon <owencadewells@gmail.com>
2024-10-30 17:36:35 -0500

    fixing the dev branch
    we had a lot of issues with the dev branch because we didn't know what we were doing but we got together in a meeting and this is the fixening.

---
+++ ai-generated-game-title/Scenes/Player.cs
@@ -0,0 +1,48 @@
+using Godot;
+using System;
+
+public partial class Player : CharacterBody2D
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+	
+	[Export]
+	private int speed = 500;
+	private int dashSpeed = 1500;
+	bool canDash = true;
+	bool dashing = false;
+
+	public void GetInput()
+	{
+		Vector2 inputDir = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+		
+		if(Input.IsActionJustPressed("dash") && canDash){
+			dashing = true;
+			canDash = false;
+			var dashTimer = GetNode<Timer>("DashTimer");
+			var dashCooldown = GetNode<Timer>("DashCooldown");
+		}
+		
+		if(dashing){
+			Velocity = inputDir * dashSpeed;
+		} else {
+			Velocity = inputDir * speed;
+		}
+	}
+	
+	public void _on_dash_timer_timeout(){
+		dashing = false;
+	}
+	
+	public void _on_dash_cooldown_timeout(){
+		canDash = true;
+	}
+
+	public override void _Process(double delta)
+	{
+		GetInput();
+		MoveAndSlide();
+	}
+}
--- ai-generated-game-title/Scripts/Unit.cs
+++
@@ -1,9 +0,0 @@
-using Godot;
-using System;
-
-public partial class Unit : Node
-{
-	[Export] public int health;
-	[Export] public int speed;
-	[Export] public int defense;
-}
